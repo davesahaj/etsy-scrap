52328a9fc7948a9adca2e6a29273ff2159a6e67b
Dave <sahajdavework@pm.me>
2023-12-31 15:14:13 +0530

    Added comments

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ import requests
 # get current Directory
 cwd = os.getcwd()
 
-# read data
+# read data from Etsy CSV file
 df = pd.read_csv('etsy/EtsyListingsDownload.csv')
-df.fillna("nan",inplace=True)
+df.fillna("nan", inplace=True)
 
 count = 0
 
-# iterate
+# iterate through each row in the DataFrame
 for idx, row in df.iterrows():
 
+    # extract item details
     desc = row['DESCRIPTION']
     price = row['PRICE']
     tags = row['TAGS']
@@ -22,37 +23,37 @@ for idx, row in df.iterrows():
 
     print('working on ' + str(idx+1) + ' item')
 
+    # create a directory for each item based on its title
+    dirName = row['TITLE'].replace('/', '|')
+    target_dir = cwd + '/data/' + dirName
 
-    dirName = row['TITLE'].replace('/','|')
-    target_dir = cwd +'/data/'+dirName
-
+    # if the directory already exists, append a random number to the directory name
     if os.path.exists(target_dir):
-        target_dir = target_dir + str(random.randrange(1,10000))
+        target_dir = target_dir + str(random.randrange(1, 10000))
 
+    # create the directory
     os.mkdir(target_dir)
 
-    if(not (desc=='nan')):
-        open(target_dir+'/description.txt', 'w').write(desc)
-    if(not (price=='nan')):
-        open(target_dir+'/price.txt', 'w').write(str(price))
-    if(not (tags=='nan')):
-        open(target_dir+'/tags.txt', 'w').write(tags)
-    if(not (material=='nan')):
-        open(target_dir+'/materials.txt', 'w').write(material)
+    # write item details to separate text files within the item's directory
+    if not (desc == 'nan'):
+        open(target_dir + '/description.txt', 'w').write(desc)
+    if not (price == 'nan'):
+        open(target_dir + '/price.txt', 'w').write(str(price))
+    if not (tags == 'nan'):
+        open(target_dir + '/tags.txt', 'w').write(tags)
+    if not (material == 'nan'):
+        open(target_dir + '/materials.txt', 'w').write(material)
 
-    
-
-    images = row[['IMAGE1','IMAGE2','IMAGE3','IMAGE4','IMAGE5','IMAGE6','IMAGE7','IMAGE8','IMAGE9','IMAGE10']]
+    # process and download images for the item
+    images = row[['IMAGE1', 'IMAGE2', 'IMAGE3', 'IMAGE4', 'IMAGE5', 'IMAGE6', 'IMAGE7', 'IMAGE8', 'IMAGE9', 'IMAGE10']]
 
     imageCount = 0
     for url in images:
-        
-        if(not (url=='nan')):
-            imageCount = imageCount+1
+
+        # if the image URL is not 'nan', download and save the image
+        if not (url == 'nan'):
+            imageCount = imageCount + 1
             extension = url.split('.')[-1]
             r = requests.get(url, allow_redirects=True)
-            open(target_dir+'/'+str(imageCount)+'.'+extension, 'wb').write(r.content)
-            r.close()    
-
-
-
+            open(target_dir + '/' + str(imageCount) + '.' + extension, 'wb').write(r.content)
+            r.close()
